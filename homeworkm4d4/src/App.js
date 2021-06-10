a84185af8f4354a024437949cb847704782da9ed
hasham zafar <hasham307@gmail.com>
2021-06-10 16:04:38 +0200

    hw
    ...

--- homeworkm4d4/src/App.js
+++ homeworkm4d4/src/App.js
@@ -6,17 +6,24 @@ import SingleBook from './component/SingleBook';
 import books from './data/books.json'
 import BookList from './component/BookList';
 import Search from './component/Search';
+import Navbar from './component/Navbar'
+import Registration from './component/Registration';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+   
+      <Navbar/>
       <Search/>
      <WarningSign title="exercise 1"/>
      <MyBadge color='success'/>
-
-     <SingleBook img = {books[0].img} title ={books[0].title}/>
-     <BookList books={books}/>
-    </div>
+      <Route path="/Rigistration" exact render={() => (<Registration/>)}/>
+      <Route path="/SingleBook" exact render={() => (<SingleBook img = {books[0].img} title ={books[0].title}/>)}/>
+      <Route path="/BookList" exact render={() => (<BookList books={books}/>)}/>
+     
+   
+    </Router>
   );
 }
 
